4ef51ad709e2e85953a4179e86c1acf3a8bb2f0f
Ameer Mohammed Ameen <ameerapex496@gmail.com>
2024-02-08 19:52:43 +0100

    task is finished

--- src/javascript.js
+++ src/javascript.js
@@ -1,2 +1,8 @@
+// if the favo button is clicked, i want to see the value in console.log of the input
+// so if clicked favo => anything in input value to be shown in console log
 
+// selecte element :
+const numberinput = document.getElementById("number-input");
+const favouriteBtn = document.getElementById("favourite-Btn");
 
+// your function:
