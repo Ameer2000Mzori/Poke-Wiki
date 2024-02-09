d2197acdbc4dd489e2ad20571b1ff4aed4879dec
Ameer Mohammed Ameen <ameerapex496@gmail.com>
2024-02-09 18:51:56 +0100

    cleaned the javascript code and added a task in task file

--- src/javascript.js
+++ src/javascript.js
@@ -1,13 +1,1 @@
-// if the favo button is clicked, i want to see the value in console.log of the input
-// so if clicked favo => anything in input value to be shown in console log
-
-// Select elements:
-const numberInput = document.getElementById("number-input");
-const favouriteBtn = document.getElementById("favourite-Btn");
-
-favouriteBtn.addEventListener("click", () => {
- 
-  const inputValue = numberInput.value;
-  console.log(`The value inside the number input is: ${inputValue}`);
-});
 
