baa1c7f3e32d35857db878b20bc3ee6dd11a9ddb
sardar Mohamad Rashed <131914394+sardarMrashed@users.noreply.github.com>
2024-02-09 15:39:03 +0300

    When give you click it will be console.log()

--- src/javascript.js
+++ src/javascript.js
@@ -1,12 +1,13 @@
 // if the favo button is clicked, i want to see the value in console.log of the input
 // so if clicked favo => anything in input value to be shown in console log
 
-// selecte element :
-const numberinput = document.getElementById("number-input");
+// Select elements:
+const numberInput = document.getElementById("number-input");
 const favouriteBtn = document.getElementById("favourite-Btn");
-// const header1 = document.getElementsByClassName("header")[0];
 
 favouriteBtn.addEventListener("click", () => {
+ 
+  const inputValue = numberInput.value;
+  console.log(`The value inside the number input is: ${inputValue}`);
+});
 
-  console.log(`When you click no favourite it will count numbers of clicks ${numberInput}`);
-})
